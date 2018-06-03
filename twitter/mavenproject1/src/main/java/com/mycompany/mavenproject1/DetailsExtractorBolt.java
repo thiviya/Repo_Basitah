208ab206568c7b42762979bbff9305e08d87b0e3
nurul_basitah <36689022+nurulbasitah@users.noreply.github.com>
2018-06-03 22:12:20 +0800

    Update DetailsExtractorBolt.java

--- twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/DetailsExtractorBolt.java
+++ twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/DetailsExtractorBolt.java
@@ -23,14 +23,10 @@ public abstract class DetailsExtractorBolt implements IRichBolt{
  
     @Override
     public void cleanup() {
-        // TODO Auto-generated method stub
+    
  
     }
  
-    /* (non-Javadoc)
-     * @see backtype.storm.task.IBolt#execute(backtype.storm.tuple.Tuple)
-     * This emits the tweet information.
-     */
     @Override
     public void execute(Tuple tuple) {
         Status status = (Status)tuple.getValueByField("status");
@@ -48,18 +44,11 @@ public abstract class DetailsExtractorBolt implements IRichBolt{
         collector.emit(new Values(status.getUser().getScreenName(), status.getUser().getName(),
             status.getId(), url, actual_url, status.getText(), status.getCreatedAt()));
     }
-    /*
-     * This saves the OutputCollector as an instance variable to be used to emit tuples.
-     */
+    
     public void prepare(Map arg0, TopologyContext arg1, OutputCollector collector) {
         this.collector = collector;        
     }
- 
-    /* (non-Javadoc)
-     * @see backtype.storm.topology.IComponent#declareOutputFields(backtype.storm.topology.OutputFieldsDeclarer)
-     * 
-     * Declares the output fields.
-     */
+
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         declarer.declare(new Fields("screenName", "userName", "statusID", 
@@ -72,9 +61,7 @@ public abstract class DetailsExtractorBolt implements IRichBolt{
         return null;
     }
  
-    /**
-     * @param url
-     */
+   
     private String getRedirectedurl(String url) {
         HttpURLConnection con;
         try {
