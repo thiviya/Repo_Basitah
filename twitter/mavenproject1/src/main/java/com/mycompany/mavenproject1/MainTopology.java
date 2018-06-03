ae7edbfe276798e5359ecce13d6348a4f22d2842
nurul_basitah <36689022+nurulbasitah@users.noreply.github.com>
2018-06-03 22:10:59 +0800

    Update MainTopology.java

--- twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/MainTopology.java
+++ twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/MainTopology.java
@@ -20,29 +20,23 @@ import org.apache.storm.tuple.Fields;
 public class MainTopology {
  
     public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
-        // TopologyBuilder instance. Defines the data flow between the components in the topology.
+       
         TopologyBuilder topologyBuilder = new TopologyBuilder();    
-        // Register the twitter spout and assign it a unique id.
+       
         topologyBuilder.setSpout(ApplicationConstants.TWITTER_SPOUT_ID, new TwitterSpout() {});    
-        /*
-         *  TwitterSpout ----> DetailsExtractorBolt
-         *  Assigned parallelism 2 for DetailsExtractorBolt.
-         */
+      
         topologyBuilder.setBolt(ApplicationConstants.DETAILS_BOLT_ID, new DetailsExtractorBolt() {}, 2)
         .shuffleGrouping(ApplicationConstants.TWITTER_SPOUT_ID);    
-        /*
-         *  TwitterSpout ----> RetweetDetailsExtractorBolt
-         *  Assigned parallelism 2 for RetweetDetailsExtractorBolt.
-         */        
+               
         topologyBuilder.setBolt(ApplicationConstants.RETWEET_DETAILS_BOLT_ID, new RetweetDetailsExtractorBolt() {}, 2)
         .shuffleGrouping(ApplicationConstants.TWITTER_SPOUT_ID);    
-        // Join DetailsExtractorBolt and RetweetDetailsExtractorBolt ----> FileWriterBolt
+     
         topologyBuilder.setBolt(ApplicationConstants.FILE_WRITER_BOLT_ID, new FileWriterBolt())
         .fieldsGrouping(ApplicationConstants.DETAILS_BOLT_ID, new Fields("title"))
         .fieldsGrouping(ApplicationConstants.RETWEET_DETAILS_BOLT_ID, new Fields("title"));    
-        // Config instance. It defines topology's runtime behavior.
+      
         Config config = new Config();    
-        // To run storm in local mode.
+       
  
         if (args != null && args.length > 0) {
               config.setNumWorkers(1);
@@ -50,9 +44,9 @@ public class MainTopology {
             }
             else {
                 final LocalCluster cluster = new LocalCluster();
-                // deploy topology in local mode.
+          
                 cluster.submitTopology(ApplicationConstants.TOPOLOGY_NAME, config, topologyBuilder.createTopology());
-                // This method will kill the topology while shutdown the JVM.
+             
                 Runtime.getRuntime().addShutdownHook(new Thread()    {
                     @Override
                     public void run()    {
