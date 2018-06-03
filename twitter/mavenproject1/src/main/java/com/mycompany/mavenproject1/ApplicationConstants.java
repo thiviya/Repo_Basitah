c6264f46a021abf2477efd10808f40a06b789776
nurul_basitah <36689022+nurulbasitah@users.noreply.github.com>
2018-06-03 22:05:14 +0800

    Update ApplicationConstants.java

--- twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/ApplicationConstants.java
+++ twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/ApplicationConstants.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.mavenproject1;
 
 /**
@@ -11,20 +7,21 @@ package com.mycompany.mavenproject1;
  */
 public class ApplicationConstants {
  
-    // Twitter application specific secret constants.   
+  
     public static final String CONSUMER_KEY_KEY = "G1vsi5YJdGqxAeeatvoQ9X3UB";
     public static final String CONSUMER_SECRET_KEY = "6S4FN4D4mkbdB5bLjeIBVwowRPT8JuIooSBM9iYvUVqLCZ7onW";
     public static final String ACCESS_TOKEN_KEY = "******-w46iJSxP8Pwo6xYiYANu49lJJnhz5i";
     public static final String ACCESS_TOKEN_SECRET_KEY = "FzEjaKg1tkJ9vpm2mGTLFUnG6yT36ZpciBvTLSuORZqtG";
  
-    // constants
+  
     public static final String NOT_AVAILABLE = "Not Available";
     public static final String EMPTY = "";
  
-    // Topology Constants 
+
     public static final String TOPOLOGY_NAME = "twitter-topology";
     public static final String TWITTER_SPOUT_ID = "twitterSpout";
     public static final String DETAILS_BOLT_ID = "detailsExtractorBolt";
     public static final String RETWEET_DETAILS_BOLT_ID = "retweetDetailsExtractorBolt";
     public static final String FILE_WRITER_BOLT_ID = "fileWriterBolt";
+   
 }
